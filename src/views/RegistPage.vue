3945a3ef21441a9fbad21d71e800dcb4b87aaa96
kimking1232 <christian.renaldo18@gmail.com>
2023-07-16 15:26:04 +0700

    16-07-2023

--- src/views/RegistPage.vue
+++ src/views/RegistPage.vue
@@ -17,81 +17,84 @@
           </h1>
           <p>Buat akun untuk melakukan pemesanan</p>
 
-          <form>
-                      <div class="row mt-5">
-                        <div class="col-lg-6">
-                          <label for=""><b>Username</b></label>
-                          <input
-                            type="text"
-                            class="form-control"
-                            placeholder="Masukkan Username"
-                            aria-label="Username"
-                            v-model="register.userName"
-                          />
-                        </div>
-                        <div class="col-lg-6">
-                          <label for=""><b>Nomer Telepon/WA</b></label>
-                          <input
-                            type="text"
-                            class="form-control"
-                            placeholder="Masukan Nomer Telepon"
-                            aria-label="Nomer Telepon"
-                            v-model="register.noWA"
-                          />
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col-lg-6">
-                          <br />
-                          <label for=""><b>Email</b></label>
-                          <input
-                            type="text"
-                            class="form-control"
-                            placeholder="Masukkan Email"
-                            aria-label="Email"
-                          />
-                        </div>
-                        <div class="col-lg-6">
-                          <br />
-                          <label for=""><b>Password</b></label>
-                          <input
-                            type="password"
-                            class="form-control"
-                            placeholder="Masukan Password"
-                            aria-label="password"
-                          />
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col">
-                          <br />
-                          <label for=""><b>Alamat</b></label>
-                          <textarea 
-                            class="form-control"
-                            placeholder="Masukan Alamat"
-                            aria-label="Alamat">
-                          </textarea>
-                        </div>
-                      </div>
-                      <div class="row mt-3">
-                        <div class="col-lg-6">
-                          <input
-                            type="checkbox"
-                            class="form-check-input"
-                            id="exampleCheck1"
-                          />
-                          <label class="form-check-label" for="exampleCheck1"
-                            >Ingat Saya</label
-                          >
-                          <br />
-                          <router-link to="/LPlogged"
-                            ><button class="btn btn-primary">Daftar</button></router-link>
-                          
-                            <br>
-            
-                          <router-link to="/LoginPage">Sudah punya akun? <b>Masuk</b></router-link>
-                        </div>
-                      </div>
+          <form v-on:submit.prevent="postRegister">
+            <div class="row mt-5">
+              <div class="col-lg-6">
+                <label for=""><b>Nama Lengkap</b></label>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Masukkan Nama Lengkap"
+                  aria-label="Username"
+                  v-model="register.name"
+                />
+              </div>
+              <div class="col-lg-6">
+                <label for=""><b>Nomer Telepon/WA</b></label>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Masukan Nomer Telepon"
+                  aria-label="Nomer Telepon"
+                  v-model="register.telephone"
+                />
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-lg-6">
+                <br />
+                <label for=""><b>Email</b></label>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Masukkan Email"
+                  aria-label="Email"
+                  v-model="register.email"
+                />
+              </div>
+              <div class="col-lg-6">
+                <br />
+                <label for=""><b>Password</b></label>
+                <input
+                  type="password"
+                  class="form-control"
+                  placeholder="Masukan Password"
+                  aria-label="password"
+                  v-model="register.password"
+                />
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <br />
+                <label for=""><b>Alamat</b></label>
+                <textarea
+                  class="form-control"
+                  placeholder="Masukan Alamat"
+                  aria-label="Alamat"
+                  v-model="register.address"
+                >
+                </textarea>
+              </div>
+            </div>
+            <div class="row mt-3">
+              <div class="col-lg-6">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  id="exampleCheck1"
+                />
+                <label class="form-check-label" for="exampleCheck1"
+                  >Ingat Saya</label
+                >
+                <br />
+                <button class="btn btn-primary">Daftar</button>
+                <br />
+                <router-link to="/LoginPage"
+                  >Sudah punya akun? <b>Masuk</b></router-link
+                >
+              </div>
+            </div>
           </form>
         </div>
 
@@ -102,14 +105,14 @@
     </div>
 
     <!-- footer -->
-    <FooterComp/>
+    <FooterComp />
     <!-- footer -->
   </div>
 </template>
 
 <script>
 // import components
-import FooterComp from '../components/FooterComp.vue'
+import FooterComp from "../components/FooterComp.vue";
 
 export default {
   name: "RegistPage",
@@ -118,7 +121,13 @@ export default {
   },
   data() {
     return {
-      register:[],
+      register: {},
+    };
+  },
+  methods: {
+    postRegister()
+    {
+      console.log(this.register)
     }
   }
 };
@@ -135,12 +144,12 @@ export default {
 }
 
 #RegistPage button {
-    margin-top: 30px;
-    margin-bottom: 50px;
+  margin-top: 30px;
+  margin-bottom: 50px;
 }
 
 #RegistPage .bg-register {
-    width: 100%;
-    margin-top: 150px;
+  width: 100%;
+  margin-top: 150px;
 }
 </style>
